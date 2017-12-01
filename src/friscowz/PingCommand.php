a9ea6e576bd8cad40b78c52658b604c5e208fa5a
FRISCOWZMCPE <ilyesou.cherfaoui.18@gmail.com>
2017-12-01 03:23:11 +0200

    Add files via upload

---
+++ src/friscowz/PingCommand.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: FRISCOWZ
+ * Date: 12/1/2017
+ * Time: 2:06 AM
+ */
+
+namespace friscowz;
+
+use pocketmine\command\CommandSender;
+use pocketmine\command\PluginCommand;
+use pocketmine\Player;
+
+class PingCommand extends PluginCommand
+{
+    /**
+     * PingCommand constructor.
+     * @param Pinger $plugin
+     */
+    public function __construct(Pinger $plugin)
+    {
+        parent::__construct("ping", $plugin);
+        $this->setAliases(["ms", "latency", "pinger"]);
+    }
+
+    /**
+     * @param CommandSender $sender
+     * @param string $commandLabel
+     * @param array $args
+     * @return bool|mixed|void
+     */
+    public function execute(CommandSender $sender, string $commandLabel, array $args)
+    {
+        if($sender instanceof Player){
+            $ping = $sender->getPing();
+            $message = str_replace("{ping}", $ping, Pinger::getConfigFile()->get("Message"));
+            $sender->sendMessage($message);
+        }
+    }
+}
---
+++ src/friscowz/Pinger.php
@@ -0,0 +1,75 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: FRISCOWZ
+ * Date: 12/1/2017
+ * Time: 2:05 AM
+ */
+
+namespace friscowz;
+
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\utils\Config;
+use pocketmine\utils\TextFormat;
+
+class Pinger extends PluginBase
+{
+    const CONFIG_FILE = "config.yml";
+    const AUTHOR = "@OGFris";
+
+    public static $instance;
+    public static $config;
+
+    public function onEnable()
+    {
+        self::setInstance($this);
+        if (!is_dir($this->getDataFolder())) @mkdir($this->getDataFolder());
+        if (file_exists($this->getDataFolder() . self::CONFIG_FILE)) {
+            $config = new Config($this->getDataFolder() . self::CONFIG_FILE, Config::YAML);
+            self::setConfigFile($config);
+        } else {
+            $config = new Config($this->getDataFolder() . self::CONFIG_FILE, Config::YAML, ["Message" => "§aYour ping is {ping}ms!"]);
+            self::setConfigFile($config);
+        }
+        $this->registerCommands();
+        $this->getLogger()->info(TextFormat::AQUA . "Pinger plugin successfully enabled! (made by " . self::AUTHOR .")");
+    }
+
+    /**
+     * @return Config
+     */
+    public static function getConfigFile() : Config
+    {
+        return self::$config;
+    }
+
+    /**
+     * @param Config $config
+     */
+    public static function setConfigFile(Config $config)
+    {
+        self::$config = $config;
+    }
+
+    /**
+     * @return Pinger
+     */
+    public static function getInstance() : Pinger
+    {
+        return self::$instance;
+    }
+
+    /**
+     * @param Pinger $instance
+     */
+    public static function setInstance(Pinger $instance)
+    {
+        self::$instance = $instance;
+    }
+
+    public function registerCommands()
+    {
+        $this->getServer()->getCommandMap()->register("ping", new PingCommand($this));
+    }
+}
